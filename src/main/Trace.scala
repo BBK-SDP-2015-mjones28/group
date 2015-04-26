9f8f53509aab2d45e2d977c692f69ce388c42176
mikiejones <mikiej_boy@hotmail.com>
2015-04-26 15:40:27 +0100

    Final group work effort
    Tidier version - all working

--- src/main/Trace.scala
+++ src/main/Trace.scala
@@ -30,36 +30,12 @@ object Trace {
 
   def render(scene: Scene, outfile: String, width: Int, height: Int) = {
     val image = new Image(width, height)
-
-    // Init the coordinator -- must be done before starting it.
-//     import akka.actor.{Props, ActorSystem, Actor}
-//     val system = ActorSystem("MySystem")
-//     val actorRef = system.actorOf(Props[Coordinator], name = "ActorRay")
-     
+    
      
      Coordinator.init(image, outfile)
-//     
-//     val system2 = ActorSystem("SystemRay")
-//     val actorRef2Path = system2./("ActorRay")
-//     val actorRef2 = system2.actorSelection(actorRef2Path)
-//     actorRef ! "Test"
-//      actorRef2 ! "Test"
 
-     
-     //actorRef2 ! "Test"
-     
-     // Coordinator.init(image, outfile)
-
-    //   ! init(image, outfile)
-    // TODO: Start the Coordinator actor.
 
     scene.traceImage(width, height)
 
-    // TODO:
-    // This one is tricky--we can't simply send a message here to print
-    // the image, since the actors started by traceImage haven't necessarily
-    // finished yet.  Maybe print should be called elsewhere?
-    
-    // Coordinator.print
   }
 }
